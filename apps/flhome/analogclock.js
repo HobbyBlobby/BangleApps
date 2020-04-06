479baeb1471d8e33a15cc4289f650f826eabb1d0
Felix Lemke <fl@informatik-dv.com>
2020-04-06 09:19:53 +0200

    minor corections

--- apps/flhome/analogclock.js
+++ apps/flhome/analogclock.js
@@ -1,6 +1,6 @@
 (function () {
     function draw() {
         bufMain.drawString("Hello Analog", bufMain.height / 2, 10);
-        bufHeighlight.drawString("Second Text", bufMain.height/2+ 10, 10)
+        bufHeighlight.drawString("Second Text", bufMain.height/2+ 10, 10);
     }
 }());
