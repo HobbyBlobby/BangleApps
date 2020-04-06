81d1f37dc439ccb05cbfdbeefea57ad7f9065b4f
HobbyBlobby <hobbyblobby@arcor.de>
2020-04-06 12:15:40 +0200

    Update analogclock.js
    use strict

--- apps/flhome/analogclock.js
+++ apps/flhome/analogclock.js
@@ -1,4 +1,4 @@
-(function () {
+(function () { 'use strict';
     function draw() {
         GlobalBuffer.bufMain.drawString("Hello Analog", 10, GlobalBuffer.bufMain.height / 2);
         GlobalBuffer.bufHeighlight.drawString("Second Text", 20, GlobalBuffer.bufHeighlight.height/2+ 50);
