b4a9009c5f644e1ce74e59adcd6a40bccddcd1e6
HobbyBlobby <hobbyblobby@arcor.de>
2020-04-12 13:29:59 +0200

    Buffer area no from height 40 to 240
    this allows rendreing of widgets ontop

--- apps/flhome/buffer.js
+++ apps/flhome/buffer.js
@@ -1,6 +1,6 @@
-var bufBG = Graphics.createArrayBuffer(240,240,1,{msb:true});
-var bufMain = Graphics.createArrayBuffer(240,240,1,{msb:true});
-var bufHeighlight = Graphics.createArrayBuffer(240,240,1,{msb:true});
+var bufBG = Graphics.createArrayBuffer(240,200,1,{msb:true});
+var bufMain = Graphics.createArrayBuffer(240,200,1,{msb:true});
+var bufHeighlight = Graphics.createArrayBuffer(240,200,1,{msb:true});
 
 var colBG = {"R": 0.3, "G": 0.3, "B": 0.3};
 var colMain = {"R": 0.8, "G": 1.0, "B": 0.8};
@@ -12,17 +12,17 @@ exports.flip = function () {
     g.setColor(colBG.R, colBG.G, colBG.B);
     g.drawImage({width:bufBG.getWidth(),
                  height:bufBG.getHeight(),
-                 buffer:bufBG.buffer},0,0);
+                 buffer:bufBG.buffer},0,40);
     g.setColor(colMain.R, colMain.G, colMain.B);
     g.drawImage({width:bufMain.getWidth(),
                  height:bufMain.getHeight(),
                  buffer:bufMain.buffer,
-                 transparent: 0},0,0);
+                 transparent: 0},0,40);
     g.setColor(colHeighlight.R, colHeighlight.G, colHeighlight.B);
     g.drawImage({width:bufHeighlight.getWidth(),
                  height:bufHeighlight.getHeight(),
                  buffer:bufHeighlight.buffer,
-                 transparent: 0},0,0);
+                 transparent: 0},0,40);
 }
 
 exports.clear = function () {
