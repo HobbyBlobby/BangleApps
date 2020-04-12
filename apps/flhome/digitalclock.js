cd9acf88f90088b8f6d7c660de16f61cfb555f40
HobbyBlobby <hobbyblobby@arcor.de>
2020-04-12 13:39:17 +0200

    center time and date

--- apps/flhome/digitalclock.js
+++ apps/flhome/digitalclock.js
@@ -4,13 +4,13 @@ function showTime() {
   var hours = ('00' + curDate.getHours()).slice(-2);
   var minutes = ('00' + curDate.getMinutes()).slice(-2);
   var seconds = ('00' + curDate.getSeconds()).slice(-2);
-  GlobalBuffer.bufMain.drawString(hours + ":" + minutes + ":" + seconds, 0,30);
+  GlobalBuffer.bufMain.drawString(hours + ":" + minutes + ":" + seconds, 20,30);
 
   GlobalBuffer.bufHeighlight.setFontVector(10);
   var year = '' + curDate.getFullYear();
   var month = ('00' + (curDate.getMonth() + 1)).slice(-2);
   var day = ('00' + curDate.getDate()).slice(-2);
-  GlobalBuffer.bufHeighlight.drawString(day + "." + month + "." + year, 70, 100);
+  GlobalBuffer.bufHeighlight.drawString(day + "." + month + "." + year, 90, 100);
 }
 
 
