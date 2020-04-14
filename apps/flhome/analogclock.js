4460df28d44f7560e0684563ad42462536fcb117
HobbyBlobby <hobbyblobby@arcor.de>
2020-04-14 10:47:58 +0200

    set dimensions during function call (else uncaught refference)

--- apps/flhome/analogclock.js
+++ apps/flhome/analogclock.js
@@ -1,7 +1,7 @@
-var height = GlobalBuffer.bufMain.getHeight();
-var width = GlobalBuffer.bufMain.getWidth();
+var height = 0;
+var width = 0;
 
-var radius = GlobalBuffer.bufMain.getHeight() / 2.0;
+var radius = 0;
 
 var PI = Math.acos(0) * 2;
 
@@ -56,8 +56,11 @@ function tickNumber(hour, distance) {
 
 function drawHands() {
   if (!Bangle.isLCDOn()) return;
-  // inner circle
+  height = GlobalBuffer.bufMain.getHeight();
+  width = GlobalBuffer.bufMain.getWidth();
+  radius = GlobalBuffer.bufMain.getHeight() / 2.0;	
 	
+  // inner circle
   GlobalBuffer.bufBG.drawCircle(width/2, height/2, 10);
   // draw numbers: seconds
   curDate = Date(Date.now());
