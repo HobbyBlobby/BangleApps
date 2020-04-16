c7d94cfda6ce7689b8db69fbfc1a33e85f25de5a
Felix Lemke <fl@informatik-dv.com>
2020-04-16 21:26:21 +0200

    use GlobalBuffer object

--- apps/flhome/digitalclock.js
+++ apps/flhome/digitalclock.js
@@ -4,14 +4,14 @@ function showTime() {
   var hours = ('00' + curDate.getHours()).slice(-2);
   var minutes = ('00' + curDate.getMinutes()).slice(-2);
   var seconds = ('00' + curDate.getSeconds()).slice(-2);
-  buf.setColor(3);
+  GlobalBuffer.buf.setColor(3);
   GlobalBuffer.buf.drawString(hours + ":" + minutes + ":" + seconds, 20,30);
 
   GlobalBuffer.buf.setFontVector(10);
   var year = '' + curDate.getFullYear();
   var month = ('00' + (curDate.getMonth() + 1)).slice(-2);
   var day = ('00' + curDate.getDate()).slice(-2);
-  buf.setColor(2);
+  GlobalBuffer.buf.setColor(2);
   GlobalBuffer.buf.drawString(day + "." + month + "." + year, 90, 100);
 }
 
