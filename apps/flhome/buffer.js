af234e95ae5c0020bdfc11ab573e27dd394ab96c
Felix Lemke <fl@informatik-dv.com>
2020-04-22 13:15:10 +0200

    prepare the change of color palettes

--- apps/flhome/buffer.js
+++ apps/flhome/buffer.js
@@ -3,7 +3,12 @@
 // var bufHeighlight = Graphics.createArrayBuffer(240,210,1,{msb:true});
 
 var buf = Graphics.createArrayBuffer(240,210,2,{msb:true});
-var palette = new Uint16Array([0, 0x6B6D, 0xAFFB, 0xFFFF]);
+// var palette = new Uint16Array([0, 0x6B6D, 0xAFFB, 0xFFFF]);
+var colorSchemes = [];
+colorSchemes.push(new Uint16Array([0, 0x4AE7, 0x7681, 0xFFFF]));
+colorSchemes.push(new Uint16Array([0, 0x8A08, 0xE861, 0xFFFF]));
+colorSchemes.push(new Uint16Array([0, 0x3169, 0x38D4, 0xFFFF]));
+var palette = colorSchemes[0];
 
 // var colBG = {"R": 0.3, "G": 0.3, "B": 0.3};
 // var colMain = {"R": 0.8, "G": 1.0, "B": 0.8};
