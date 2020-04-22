42206442a5134a5c848897d3f30e191947aa516a
Felix Lemke <fl@informatik-dv.com>
2020-04-22 16:10:14 +0200

    remove explicitl loaded modules

--- apps/flhome/homescreen.app.js
+++ apps/flhome/homescreen.app.js
@@ -88,9 +88,11 @@ function init() {
 function showLauncher() {
     clearInterval(HomeScreen.timerID);
     delete GlobalBuffer.buf;
-    Modules.removeAllCached();
+    Modules.removeCached("buffer.js");
+    Modules.removeCached("analogclock.js");
+    Modules.removeCached("digitalclock.js");
     g.clear();
-    g.drawString(process.memory().free.toString(), 10,10);
+    g.drawString(process.memory().free.toString(), 50,50);
     
 //     Bangle.showLauncher();
 }
